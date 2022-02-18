b0b03da2e0934a1b4870c69ff9967739283aecf1
kevinli8831 <66711517+kevinli8831@users.noreply.github.com>
2022-02-18 13:17:55 +0800

    Add files via upload
    trial particle

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import Particles from "particles.vue";
+
+
+Vue.use(Particles);
+
+
 
 Vue.config.productionTip = false
 
